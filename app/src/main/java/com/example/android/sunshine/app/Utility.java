50a2a81b966fa492275628e9ca09b7d4d78f79f8
KAMINI <jainkamini08@gmail.com>
2015-10-27 21:39:20 -0500

    notification code add

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -26,7 +26,20 @@ public class Utility {
                 .equals(context.getString(R.string.pref_units_metric));
     }
 
-    static String formatTemperature(Context context, double temperature, boolean isMetric) {
+    public static String formatTemperature(Context context, double temperature) {
+        // Data stored in Celsius by default.  If user prefers to see in Fahrenheit, convert
+        // the values here.
+        String suffix = "\u00B0";
+        if (!isMetric(context)) {
+            temperature = (temperature * 1.8) + 32;
+        }
+
+        // For presentation, assume the user doesn't care about tenths of a degree.
+        return String.format(context.getString(R.string.format_temperature), temperature);
+    }
+
+
+    /*static String formatTemperature(Context context, double temperature, boolean isMetric) {
         double temp;
         if ( !isMetric ) {
             temp = 9*temperature/5+32;
@@ -34,7 +47,7 @@ public class Utility {
             temp = temperature;
         }
         return context.getString(R.string.format_temperature, temp);
-    }
+    }*/
 
     static String formatDate(long dateInMilliseconds) {
         Date date = new Date(dateInMilliseconds);
