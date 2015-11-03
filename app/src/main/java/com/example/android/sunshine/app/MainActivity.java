7dcc741e36845776f78f56f273d165c191cfebf0
KAMINI <jainkamini08@gmail.com>
2015-11-03 15:34:19 -0600

    change the detail view acroding matrrial design

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
      * Substitute you own project number here. This project number comes
      * from the Google Developers Console.
      */
-    static final String PROJECT_NUMBER = "Your Project Number";
+    static final String PROJECT_NUMBER = "221495239775";
 
     private boolean mTwoPane;
     private String mLocation;
